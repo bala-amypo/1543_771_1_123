2225e3c5060438098611b82296d997147d2919f2
bala-amypo <bala@amypo.in>
2025-12-15 10:21:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -1,3 +1,5 @@
+import java.security.Timestamp;
+
 public class Employee {
     private Long id;
     private String fullName;
@@ -7,8 +9,62 @@ public class Employee {
     private Boolean active;
     private Timestamp createdAt;
     private Timestamp updatedAt;
-
-    public id(){
-        
+    public Employee(String fullName, String email, String department, String jobTitle, Boolean active,
+            Timestamp createdAt, Timestamp updatedAt) {
+        this.fullName = fullName;
+        this.email = email;
+        this.department = department;
+        this.jobTitle = jobTitle;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public String getJobTitle() {
+        return jobTitle;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
     }
 }
