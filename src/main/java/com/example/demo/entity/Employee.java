c7a8c2ca221fcf3c388a4ae67b11f414e9fef0af
bala-amypo <bala@amypo.in>
2025-12-15 10:16:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String department;
+    private String jobTitle;
+    private Boolean active;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public id(){
+        
+    }
+}
