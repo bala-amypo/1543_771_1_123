ebe9a8bad4af7747c746401aff8d9930cb754d66
bala-amypo <bala@amypo.in>
2025-12-18 08:39:46 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/SkillServiceimpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Skill;
+import com.example.demo.repository.SkillRepository;
+import com.example.demo.service.SkillService;
+import com.example.demo.exception.ResourceNotFoundException;
+
+import java.util.List;
+
+public class SkillServiceImpl implements SkillService {
+
+    private final SkillRepository skillRepository;
+
+    public SkillServiceImpl(SkillRepository skillRepository) {
+        this.skillRepository = skillRepository;
+    }
+
+    @Override
+    public Skill createSkill(Skill skill) {
+        return skillRepository.save(skill);
+    }
+
+    @Override
+    public Skill updateSkill(Long id, Skill skill) {
+        Skill existing = getSkillById(id);
+        skill.setId(existing.getId());
+        return skillRepository.save(skill);
+    }
+
+    @Override
+    public Skill getSkillById(Long id) {
+        return skillRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Skill not found"));
+    }
+
+    @Override
+    public List<Skill> getAllSkills() {
+        return skillRepository.findAll();
+    }
+
+    @Override
+    public void deactivateSkill(Long id) {
+        Skill skill = getSkillById(id);
+        skill.setActive(false);
+        skillRepository.save(skill);
+    }
+}
