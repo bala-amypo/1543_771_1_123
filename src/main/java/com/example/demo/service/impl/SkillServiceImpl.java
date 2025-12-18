db1bbc6fe9c44a3bf7f1aa0186b6ae2928d151c8
bala-amypo <bala@amypo.in>
2025-12-18 08:59:45 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SkillServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SkillServiceImpl.java
@@ -6,7 +6,7 @@ import com.example.demo.service.SkillService;
 import com.example.demo.exception.ResourceNotFoundException;
 
 import java.util.List;
-
+@Service
 public class SkillServiceImpl implements SkillService {
 
     private final SkillRepository skillRepository;
