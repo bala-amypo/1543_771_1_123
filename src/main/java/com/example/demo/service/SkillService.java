f36390ad36942113dfe8e22edf15043ca4ee9814
bala-amypo <bala@amypo.in>
2025-12-18 09:09:45 +0000

    User commit

--- src/main/java/com/example/demo/service/SkillService.java
+++ src/main/java/com/example/demo/service/SkillService.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
-
+import org.springframework.stereotype.Service;
 import com.example.demo.model.Skill;
 
 import java.util.List;
-
+@Service
 public interface SkillService {
 
     Skill createSkill(Skill skill);
