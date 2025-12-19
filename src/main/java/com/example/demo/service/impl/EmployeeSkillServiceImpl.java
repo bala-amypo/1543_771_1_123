55fe9a1f3300d166e93bac3f247f2131d1f1ece4
bala-amypo <bala@amypo.in>
2025-12-19 08:43:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeSkillServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeSkillServiceImpl.java
@@ -38,25 +38,25 @@ public class EmployeeSkillServiceImpl implements EmployeeSkillService {
                                 Integer yearsOfExperience) {
 
         if (yearsOfExperience == null || yearsOfExperience < 0) {
-            throw new IllegalArgumentException("Experience years");
+            throw new IllegalArgumentException("Invalid years of experience");
         }
 
         if (!VALID_PROFICIENCIES.contains(proficiencyLevel)) {
-            throw new IllegalArgumentException("Invalid proficiency");
+            throw new IllegalArgumentException("Invalid proficiency level");
         }
 
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
 
         if (!employee.isActive()) {
-            throw new IllegalArgumentException("inactive employee");
+            throw new IllegalArgumentException("Employee is inactive");
         }
 
         Skill skill = skillRepository.findById(skillId)
                 .orElseThrow(() -> new ResourceNotFoundException("Skill not found"));
 
         if (!skill.getActive()) {
-            throw new IllegalArgumentException("inactive skill");
+            throw new IllegalArgumentException("Skill is inactive");
         }
 
         EmployeeSkill employeeSkill = new EmployeeSkill();
@@ -71,20 +71,22 @@ public class EmployeeSkillServiceImpl implements EmployeeSkillService {
 
     @Override
     public List<EmployeeSkill> getSkillsForEmployee(Long employeeId) {
-        return employeeSkillRepository.findByEmployeeIdAndActiveTrue(employeeId);
+        return employeeSkillRepository
+                .findByEmployee_IdAndActiveTrue(employeeId);
     }
 
     @Override
     public List<EmployeeSkill> getEmployeesBySkill(Long skillId) {
-        return employeeSkillRepository.findBySkillIdAndActiveTrue(skillId);
+        return employeeSkillRepository
+                .findBySkill_IdAndActiveTrue(skillId);
     }
 
     @Override
     public void deactivateEmployeeSkill(Long id) {
-        EmployeeSkill es = employeeSkillRepository.findById(id)
+        EmployeeSkill employeeSkill = employeeSkillRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("EmployeeSkill not found"));
 
-        es.setActive(false);
-        employeeSkillRepository.save(es);
+        employeeSkill.setActive(false);
+        employeeSkillRepository.save(employeeSkill);
     }
 }
